f919419adf703971cd7ca46a139bc5ebcd192179
Sergei Malinin <s@malinin.dev>
2025-06-25 17:12:58 +0700

    review fixes

--- src/lib/extractCssClasses/extractCssClasses.ts
+++ src/lib/extractCssClasses/extractCssClasses.ts
@@ -1,31 +1,18 @@
-import postcss, { type Rule, type Node } from 'postcss';
+import postcss from 'postcss';
 import { extractComposedClasses } from './utils/extractComposedClasses.js';
 import { extractClassNamesFromRule } from './utils/extractClassNamesFromRule.js';
-
-const processNode = (node: Node, classNames: Set<string>): void => {
-  if (node.type === 'rule') {
-    const ruleClassNames = extractClassNamesFromRule(node as Rule);
-
-    for (const className of ruleClassNames) {
-      classNames.add(className);
-    }
-  }
-
-  if ('nodes' in node && Array.isArray(node.nodes)) {
-    for (const childNode of node.nodes) {
-      processNode(childNode, classNames);
-    }
-  }
-};
 
 export const extractCssClasses = (cssContent: string): string[] => {
   const classNames = new Set<string>();
 
   const root = postcss.parse(cssContent);
 
-  for (const node of root.nodes) {
-    processNode(node, classNames);
-  }
+  root.walkRules((rule) => {
+    const ruleClassNames = extractClassNamesFromRule(rule);
+    for (const className of ruleClassNames) {
+      classNames.add(className);
+    }
+  });
 
   const composedClasses = extractComposedClasses(root);
   for (const composedClassName of composedClasses) {
